c376d11d781513dc6ad323290b78716946e2ef43
Diego Martinez <diegofernandomartinezcalderon@gmail.com>
2023-05-24 21:31:44 -0500

    feat: :lipstick: mejora del codigo

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
 
-class persona {
-  nombre
-  edad
-  sexo
-constructor({nombre1, edad1, sexo1}) {
-  this.nombre = nombre1;
-  this.edad = edad1;
-  this.sexo = sexo1;
+
+class persona {//clase padre persona
+constructor(nombre, edad, sexo) {
+  this.nombrePersona = nombre; //se reciben los 3 parametros(nombre, edad, sexo) y los agg a las porpiedades de la clase con this. 
+  this.edadPersona = edad;
+  this.sexoPersona = sexo;
 }
-saludar1(nom){
-  document.getElementById("textarea").innerHTML = `Hola ${nom}`;
+saludar(){ //metodo
+ return "Hola " + this.nombrePersona;
 }
  }
 
+
+
+ class estudiante extends persona{ //clase hijo estudiante
+
+  constructor(nombre,edad,sexo,materia){
+    super(nombre,edad,sexo); //no es nesesario hacer el this.nombre por que esos ya están en la clase padre, solo de las nuevas del hijo
+    //en el super debo poner todos los parametros que traigo de la clase padre
+    this.materia=materia
+  }
+
+    saludarEstudiante(){
+    return super.saludar() + " Tú materia es: " + this.materia//llamo al metodo de la super clase
+    }
+ }
+
+
+
+
+
  function saludar() {
-     let nombre1 = document.getElementById("nombre").value;
-     let edad1 = document.getElementById("edad").value;
-     let sexo1 = document.getElementById("sexo").value;
-     console.log(nombre1,edad1,sexo1)
-
-
-     let persona1= new persona({nombre1,edad1,sexo1});
-     persona1.saludar1(nombre1)
+     let nombre = document.getElementById("nombre").value;
+     let edad = document.getElementById("edad").value;
+     let sexo = document.getElementById("sexo").value;
+     let materia = document.getElementById("materia").value;
      
+     //creamos la persona:
+    //  let objPersona= new persona(nombre,edad,sexo,materia);
+      // console.log(objPersona.saludar())
+
+    //  document.getElementById("textarea").innerHTML =objPersona.saludar() ;
+
+       let objestudiante =new estudiante(nombre, edad, sexo, materia);
+
+       document.getElementById("textarea").innerHTML =objestudiante.saludarEstudiante() ;
  }
 
 
